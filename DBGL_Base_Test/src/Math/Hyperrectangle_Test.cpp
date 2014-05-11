2b8096683ffb12c31a0c39e13d5b7741c5a332f4
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-05-11 12:56:08 +0200

    Added rectangle test

---
+++ DBGL_Base_Test/src/Math/Hyperrectangle_Test.cpp
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////
+/// Dragon Blaze Game Library
+///
+/// Copyright (c) 2014 by Jan Moeller
+///
+/// This software is provided "as-is" and does not claim to be
+/// complete or free of bugs in any way. It should work, but
+/// it might also begin to hurt your kittens.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef VECTOR_TEST_CPP
+#define VECTOR_TEST_CPP
+
+#include <cstdio>
+#include <cassert>
+#include <cmath>
+#include "DBGL/System/Log/Log.h"
+#include "DBGL/Math/Hyperrectangle.h"
+#include "DBGL/Math/Vector2.h"
+
+using namespace dbgl;
+
+int testHyperrectangle()
+{
+    LOG->info("Starting Hyperrectangle test suite...");
+    LOG->info("Constructors... ");
+    Hyperrectangle<float, 2> rect2d;
+    Hyperrectangle<float, 2> rect2d_2(Vec2f(1, 0.5f), Vec2f(8, 10));
+    Hyperrectangle<float, 2> rect2d_2Copy(rect2d_2);
+    Rectangle<double> rect(Vec2d(2, 2), Vec2d(42, 23));
+    assert(rect2d.getPos() == Vec2f(0, 0));
+    assert(rect2d.getExtent() == Vec2f(1, 1));
+    assert(rect2d_2.getPos() == Vec2f(1, 0.5f));
+    assert(rect2d_2.getExtent() == Vec2f(8, 10));
+    assert(rect2d_2Copy.getPos() == rect2d_2.getPos());
+    assert(rect2d_2Copy.getExtent() == rect2d_2.getExtent());
+    assert(rect.getPos() == Vec2d(2, 2));
+    assert(rect.getExtent() == Vec2d(42, 23));
+    LOG->info("OK!");
+    LOG->info("Methods... ");
+    // ...
+    LOG->info("OK!");
+    LOG->info("Operators... ");
+    // =
+    auto copy(rect2d);
+    copy = rect2d_2;
+    assert(copy.getPos() == rect2d_2.getPos());
+    assert(copy.getExtent() == rect2d_2.getExtent());
+    // ==
+    assert(copy == rect2d_2);
+    assert(rect2d_2Copy == copy);
+    // !=
+    assert(copy != rect2d);
+    LOG->info("OK!");
+    LOG->info("Done!");
+    return 0;
+}
+
+#endif
