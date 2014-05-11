bdf79df6e609d754d79d63eb0d9b5c65464da746
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-05-11 12:54:06 +0200

    Added a specialisation for rectangles

--- DBGL_Base/include/DBGL/Math/Hyperrectangle.h
+++ DBGL_Base/include/DBGL/Math/Hyperrectangle.h
@@ -12,6 +12,7 @@
 #define HYPERRECTANGLE_H_
 
 #include "Vector.h"
+#include "Vector2.h"
 
 namespace dbgl
 {
@@ -35,7 +36,7 @@ namespace dbgl
 	     * @brief Copies a hyperrectangle
 	     * @param other Hyperrectangle to copy
 	     */
-	    Hyperrectangle(Hyperrectangle const& other);
+	    Hyperrectangle(Hyperrectangle<T, D> const& other);
 	    /**
 	     * @brief Provides access to the hyperrectangles coordinates
 	     * @return Position of the hyperrectangle
@@ -82,6 +83,80 @@ namespace dbgl
 	private:
 	    Vector<T, D> m_pos;
 	    Vector<T, D> m_extent;
+    };
+
+    /**
+     * @brief Rectangle (i.e. 2 dimensions)
+     */
+    template<typename T> using Rectangle = Hyperrectangle<T, 2>;
+
+    /**
+     * @brief A rectangle (i.e. 2 dimensions)
+     */
+    template<typename T> class Hyperrectangle<T, 2>
+    {
+	public:
+	    /**
+	     * @brief Constructs a rectangle located at the origin with extent 1
+	     */
+	    Hyperrectangle();
+	    /**
+	     * @brief Constructs a rectangle from a position vector and an extent vector
+	     * @param pos Coordinates to use
+	     * @param extent Extent to use
+	     */
+	    Hyperrectangle(Vector<T, 2> const& pos, Vector<T, 2> const& extent);
+	    /**
+	     * @brief Copies a rectangle
+	     * @param other Rectangle to copy
+	     */
+	    Hyperrectangle(Hyperrectangle<T, 2> const& other);
+	    /**
+	     * @brief Provides access to the rectangles coordinates
+	     * @return Position of the rectangle
+	     */
+	    Vector2<T> const& getPos() const;
+	    /**
+	     * @brief Provides access to the rectangles coordinates
+	     * @return Position of the rectangle
+	     */
+	    Vector2<T>& pos();
+	    /**
+	     * @brief Provides access to the rectangles extent
+	     * @return Extent of the rectangle
+	     */
+	    Vector2<T> const& getExtent() const;
+	    /**
+	     * @brief Provides access to the rectangles extent
+	     * @return Extent of the rectangle
+	     */
+	    Vector2<T>& extent();
+	    /**
+	     * @brief Assigns another rectangle by copying its values
+	     * @param rhs Rectangle to copy
+	     * @return Reference to this rectangle
+	     */
+	    Hyperrectangle<T, 2>& operator=(Hyperrectangle<T, 2> const& rhs);
+	    /**
+	     * @brief Value comparison
+	     * @param rhs Rectangle to compare
+	     * @return True in case both rectangles are equal, otherwise false
+	     */
+	    bool operator==(Hyperrectangle<T, 2> const& rhs) const;
+	    /**
+	     * @brief Value comparison
+	     * @param rhs Rectangle to compare
+	     * @return False in case both rectangles are equal, otherwise true
+	     */
+	    bool operator!=(Hyperrectangle<T, 2> const& rhs) const;
+	    /**
+	     * @brief Dimensionality of the rectangle
+	     * @return Dimensionality of the rectangle
+	     */
+	    static unsigned int getDimension();
+	private:
+	    Vector2<T> m_pos;
+	    Vector2<T> m_extent;
     };
 }
 
