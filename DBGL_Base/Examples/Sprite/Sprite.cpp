7f40ed84681287645c1b34ce5af4778fd7ad490c
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-08-01 23:27:52 +0200

    Initial implementation of sprite class

---
+++ DBGL_Base/Examples/Sprite/Sprite.cpp
@@ -0,0 +1,177 @@
+//////////////////////////////////////////////////////////////////////
+/// Dragon Blaze Game Library
+///
+/// Copyright (c) 2014 by Jan Moeller
+///
+/// This software is provided "as-is" and does not claim to be
+/// complete or free of bugs in any way. It should work, but
+/// it might also begin to hurt your kittens.
+//////////////////////////////////////////////////////////////////////
+
+#include <functional>
+#include <cmath>
+#include "DBGL/System/System.h"
+#include "DBGL/System/Log/Log.h"
+#include "DBGL/System/Bitmask/Bitmask.h"
+#include "DBGL/Window/WindowManager.h"
+#include "DBGL/Window/SimpleWindow.h"
+#include "DBGL/Window/Input.h"
+#include "DBGL/Rendering/RenderContext.h"
+#include "DBGL/Rendering/Mesh/Mesh.h"
+#include "DBGL/Rendering/ShaderProgram.h"
+#include "DBGL/Rendering/Texture/Texture.h"
+#include "DBGL/Rendering/Sprite/Sprite.h"
+#include "DBGL/Rendering/Environment/Camera.h"
+#include "DBGL/Math/Vector3.h"
+#include "DBGL/Math/Utility.h"
+#include "DBGL/Math/Quaternion.h"
+
+using namespace dbgl;
+
+Window* pWnd = nullptr;
+ShaderProgram* pShader = nullptr;
+Texture* pTexture = nullptr;
+Sprite* pSprite = nullptr;
+Camera* pCam = nullptr;
+Mat4f view{}, projection{};
+float mouseSpeed = 1.5, moveSpeed = 2.5;
+
+void inputCallback(Window::InputEventArgs const& args)
+{
+    // If the tab key has just been released
+    if(args.input.isReleased(Input::KEY_TAB))
+    {
+	// Check if the currently shown rectangle contains the whole texture
+	if(pSprite->getRect().right(0) == pTexture->getWidth() && pSprite->getRect().right(1) == pTexture->getHeight() &&
+		pSprite->getRect().left(0) == 0 && pSprite->getRect().left(1) == 0)
+	{
+	    // Crop to the upper part
+	    Rectangle<unsigned int> rect = pSprite->getRect();
+	    rect.pos().y() = 142;
+	    pSprite->setRect(rect);
+	}
+	else
+	{
+	    Rectangle<unsigned int> rect(Vector2<unsigned int>{0, 0}, Vector2<unsigned int>{pTexture->getWidth(), pTexture->getHeight()});
+	    pSprite->setRect(rect);
+	}
+    }
+}
+
+void framebufferResizeCallback(Window::FramebufferResizeEventArgs const& args)
+{
+    // Update projection matrix if the windows was rescaled
+    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
+	    float(args.width) / args.height, pCam->getNear(), pCam->getFar());
+}
+
+void updateCallback(Window::UpdateEventArgs const& args)
+{
+    auto deltaTime = args.deltaTime;
+
+    // Update mouse
+    double x=0, y=0;
+    pWnd->getCursorPos(x, y);
+    float horizontal = deltaTime * mouseSpeed
+	    * float(pWnd->getFrameWidth() / 2 - x);
+    float vertical = deltaTime * mouseSpeed
+	    * float(pWnd->getFrameHeight() / 2 - y);
+    pCam->rotate(horizontal, -vertical);
+    // Camera vectors
+    Vec3f direction = pCam->rotation() * Vec3f(0, 0, 1);
+    Vec3f right = pCam->rotation() * Vec3f(-1, 0, 0);
+    // Reset mouse position to center of the screen
+    pWnd->setCursorPos(pWnd->getFrameWidth() / 2, pWnd->getFrameHeight() / 2);
+
+    // Update view matrix
+    view = Mat4f::makeView(pCam->position(), pCam->rotation() * Vec3f(0, 0, 1),
+	    pCam->rotation() * Vec3f(0, 1, 0));
+
+    // Update keyboard
+    if (pWnd->getKey(Input::Key::KEY_W) == Input::KeyState::DOWN)
+	pCam->position() += direction * deltaTime * moveSpeed;
+    if (pWnd->getKey(Input::Key::KEY_A) == Input::KeyState::DOWN)
+	pCam->position() -= right * deltaTime * moveSpeed;
+    if (pWnd->getKey(Input::Key::KEY_S) == Input::KeyState::DOWN)
+	pCam->position() -= direction * deltaTime * moveSpeed;
+    if (pWnd->getKey(Input::Key::KEY_D) == Input::KeyState::DOWN)
+	pCam->position() += right * deltaTime * moveSpeed;
+    if (pWnd->getKey(Input::Key::KEY_E) == Input::KeyState::DOWN)
+	pCam->position() += Vec3f(0, 1, 0) * deltaTime * moveSpeed;
+    if (pWnd->getKey(Input::Key::KEY_Q) == Input::KeyState::DOWN)
+	pCam->position() -= Vec3f(0, 1, 0) * deltaTime * moveSpeed;
+}
+
+void renderCallback(Window::RenderEventArgs const& args)
+{
+    auto rc = args.rc;
+
+    // Instruct shader
+    pShader->use();
+    // Check for uniforms
+    GLint mvpId = pShader->getDefaultUniformHandle(ShaderProgram::Uniform::MVP);
+    GLint itmvId = pShader->getDefaultUniformHandle(ShaderProgram::Uniform::ITMV);
+    if (mvpId <= 0 || itmvId <= 0)
+	return;
+    // Diffuse texture
+    GLint diffuseId = pShader->getDefaultUniformHandle(
+	    ShaderProgram::TEX_DIFFUSE);
+    if (diffuseId >= 0)
+    {
+	// Bind diffuse texture to unit 0
+	pShader->bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, pTexture->getHandle());
+	pShader->setUniformSampler(diffuseId, 0);
+    }
+
+    // Sprite will be drawn in the center of the world
+    // The model matrix equals the identity matrix and can thus be left out
+    Mat4f mvp = projection * view;
+    // Send to shader
+    pShader->setUniformFloatMatrix4Array(mvpId, 1, GL_FALSE,
+	    mvp.getDataPointer());
+    pShader->setUniformFloatMatrix4Array(itmvId, 1, GL_FALSE,
+	    view.getDataPointer());
+    rc->draw(*(pSprite->getMesh()));
+}
+
+int main()
+{
+    LOG.setLogLevel(Log::Level::DBG);
+    // Init
+    dbgl::initialize();
+    // Create window
+    pWnd = WindowManager::get()->createWindow<SimpleWindow>();
+    // Initialize it
+    pWnd->init();
+    // Add a camera
+    pCam = new Camera {Vec3f(0, 0, 3), Vec3f(0, 0, -1), Vec3f(0, 1, 0), pi_4(), 0.1, 100};
+    view = Mat4f::makeView(pCam->position(),
+    	    pCam->rotation() * Vec3f(0, 0, 1), pCam->rotation() * Vec3f(0, 1, 0));
+    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
+	float(pWnd->getFrameWidth()) / pWnd->getFrameHeight(), pCam->getNear(), pCam->getFar());
+    // Load shader and texture
+    pShader = ShaderProgram::createSimpleShader();
+    pTexture = Texture::load(Texture::TGA, "../common/DBGL_Logo_512.tga");
+    pSprite = new Sprite{pTexture};
+    // Add update- and render callback so we can draw the mesh
+    pWnd->addUpdateCallback(std::bind(&updateCallback, std::placeholders::_1));
+    pWnd->addRenderCallback(std::bind(&renderCallback, std::placeholders::_1));
+    pWnd->addFramebufferResizeCallback(std::bind(&framebufferResizeCallback, std::placeholders::_1));
+    pWnd->addInputCallback(std::bind(&inputCallback, std::placeholders::_1));
+    // Show window
+    pWnd->show();
+    // Run update loop
+    while (dbgl::isRunning())
+    {
+	dbgl::update();
+    }
+    // Clean up
+    delete pShader;
+    delete pTexture;
+    delete pSprite;
+    delete pCam;
+    // Free remaining internal resources
+    dbgl::terminate();
+    return 0;
+}
+
---
+++ DBGL_Base/include/DBGL/Rendering/Sprite/Sprite.h
@@ -0,0 +1,101 @@
+//////////////////////////////////////////////////////////////////////
+/// Dragon Blaze Game Library
+///
+/// Copyright (c) 2014 by Jan Moeller
+///
+/// This software is provided "as-is" and does not claim to be
+/// complete or free of bugs in any way. It should work, but
+/// it might also begin to hurt your kittens.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef SPRITE_H_
+#define SPRITE_H_
+
+#include "DBGL/Rendering/Texture/Texture.h"
+#include "DBGL/Rendering/Mesh/Mesh.h"
+#include "DBGL/Math/Hyperrectangle.h"
+
+namespace dbgl
+{
+    class Sprite
+    {
+	public:
+	    /**
+	     * @brief Constructor
+	     * @param tex Texture to get the sprite from
+	     */
+	    Sprite(Texture* tex);
+	    /**
+	     * @brief Destructor
+	     */
+	    ~Sprite();
+	    /**
+	     * @brief Copy constructor
+	     * @param other Sprite to copy
+	     */
+	    Sprite(Sprite const& other);
+	    /**
+	     * @brief Assignment operator
+	     * @param other Sprite to assign
+	     * @return Reference to this
+	     */
+	    Sprite& operator=(Sprite const& other);
+	    /**
+	     * @brief Returns the texture used for this sprite
+	     * @return The texture used for this sprite
+	     */
+	    Texture* getTexture() const;
+	    /**
+	     * @brief Replaces the texture used for this sprite
+	     * @param tex New texture
+	     * @note Crops Rect to the new texture size if necessary
+	     */
+	    void setTexture(Texture* tex);
+	    /**
+	     * @brief Returns the rectangle on the used texture used for this sprite
+	     * @return Rectangle on the used texture used for this sprite
+	     */
+	    Rectangle<unsigned int> const& getRect() const;
+	    /**
+	     * @brief Replaces the rectangle on the used texture
+	     * @param rect New rectangle
+	     */
+	    void setRect(Rectangle<unsigned int> rect);
+	    /**
+	     * @brief Returns the mesh fitting to the currently chosen rectangle
+	     * @return Mesh fitting to the currently chosen rectangle
+	     */
+	    Mesh* getMesh() const;
+	private:
+	    /**
+	     * @brief Recomputes mesh proportions and UVs
+	     */
+	    void recomputeMesh();
+	    /**
+	     * @brief Validates the rectangle and crops it to the texture size if needed
+	     */
+	    void validateRect();
+
+
+	    /**
+	     * @brief Texture to get sprite from
+	     */
+	    Texture* m_pTexture = nullptr;
+	    /**
+	     * @brief Rectangle to display
+	     */
+	    Rectangle<unsigned int> m_rect;
+	    /**
+	     * @brief Mesh to display the sprite on
+	     */
+	    Mesh* m_pMesh = nullptr;
+	    /**
+	     * @brief Indices of the corner vertices
+	     */
+	    unsigned int tl = 5, tr = 5, ll = 5, lr = 5;
+    };
+}
+
+
+
+#endif /* SPRITE_H_ */
