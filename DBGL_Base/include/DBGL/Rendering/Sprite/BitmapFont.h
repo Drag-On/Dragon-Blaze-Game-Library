46006d37a1fbe41e4545c2ea288b98e0ba0bfdec
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-08-03 19:02:57 +0200

    Started to implement bitmap fonts

---
+++ DBGL_Base/include/DBGL/Rendering/Sprite/BitmapFont.h
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////
+/// Dragon Blaze Game Library
+///
+/// Copyright (c) 2014 by Jan Moeller
+///
+/// This software is provided "as-is" and does not claim to be
+/// complete or free of bugs in any way. It should work, but
+/// it might also begin to hurt your kittens.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef BITMAPFONT_H_
+#define BITMAPFONT_H_
+
+#include <string>
+#include <fstream>
+#include "Sprite.h"
+#include "DBGL/Rendering/Texture/Texture.h"
+#include "DBGL/System/Log/Log.h"
+
+namespace dbgl
+{
+    /**
+     * @brief Contains functionality to load a bitmap font from file and use it to draw text to the screen
+     * @details The file to load has to be in the bff2 format, written by the tool "CBFG" (http://www.codehead.co.uk/cbfg/)
+     */
+    class BitmapFont
+    {
+	public:
+	    /**
+	     * @brief Constructor
+	     * @param filename Path of the font file to load
+	     */
+	    BitmapFont(std::string const& filename);
+	    /**
+	     * @brief Copy constructor
+	     * @param other BitmapFont to copy
+	     */
+	    BitmapFont(BitmapFont const& other);
+	    /**
+	     * @brief Destructor
+	     */
+	    ~BitmapFont();
+	    /**
+	     * @brief Assignment operator
+	     * @param other BitmapFont to assign
+	     * @return Reference to this BitmapFont
+	     */
+	    BitmapFont& operator=(BitmapFont const& other);
+	private:
+	    /**
+	     * @brief loads the font file
+	     * @param filename Path of the font
+	     * @return True if everything went right, otherwise false
+	     */
+	    bool load(std::string const& filename);
+
+	    static const unsigned int headerSize = 20;
+	    Texture* m_pTexture;
+	    Sprite* m_pSprite;
+	    struct FileHeader
+	    {
+		public:
+		    unsigned char id1 = 0, id2 = 0;
+		    int imgWidth = 0, imgHeight = 0, cellWidth = 0, cellHeight = 0;
+		    unsigned char bpp = 0, base = 0;
+	    } m_header;
+	    char m_widths[256];
+    };
+}
+
+#endif /* BITMAPFONT_H_ */
---
+++ DBGL_Base/src/DBGL/Rendering/Sprite/BitmapFont.cpp
@@ -0,0 +1,126 @@
+//////////////////////////////////////////////////////////////////////
+/// Dragon Blaze Game Library
+///
+/// Copyright (c) 2014 by Jan Moeller
+///
+/// This software is provided "as-is" and does not claim to be
+/// complete or free of bugs in any way. It should work, but
+/// it might also begin to hurt your kittens.
+//////////////////////////////////////////////////////////////////////
+
+#include "DBGL/Rendering/Sprite/BitmapFont.h"
+
+namespace dbgl
+{
+    BitmapFont::BitmapFont(std::string const& filename)
+    {
+	load(filename);
+    }
+
+    BitmapFont::BitmapFont(BitmapFont const& other)
+    {
+	m_pTexture = new Texture{*other.m_pTexture};
+	m_pSprite = new Sprite{*other.m_pSprite};
+    }
+
+    BitmapFont::~BitmapFont()
+    {
+	delete m_pTexture;
+	delete m_pSprite;
+    }
+
+    BitmapFont& BitmapFont::operator=(BitmapFont const& other)
+    {
+	if(this != &other)
+	{
+	    delete m_pTexture;
+	    delete m_pSprite;
+	    m_pTexture = new Texture{*other.m_pTexture};
+	    m_pSprite = new Sprite{*other.m_pSprite};
+	}
+	return *this;
+    }
+
+    bool BitmapFont::load(std::string const& filename)
+    {
+	// Variables to store data in
+	char headDat[headerSize]{};
+	char* img = nullptr;
+	// Stream to read from
+	std::fstream in{};
+	// Open file
+	in.open(filename.c_str(), std::ios_base::binary | std::ios_base::in);
+	// Everything alright?
+	if(!in.is_open())
+	    return false;
+	// Read in header
+	in.read(&(headDat[0]), headerSize);
+	m_header.id1 = headDat[0];
+	m_header.id2 = headDat[1];
+	m_header.imgWidth = (headDat[2]<<0) | (headDat[3]<<8) | (headDat[4]<<16) | (headDat[5]<<24);
+	m_header.imgHeight = (headDat[6]<<0) | (headDat[7]<<8) | (headDat[8]<<16) | (headDat[9]<<24);
+	m_header.cellWidth = (headDat[10]<<0) | (headDat[11]<<8) | (headDat[12]<<16) | (headDat[13]<<24);
+	m_header.cellHeight = (headDat[14]<<0) | (headDat[15]<<8) | (headDat[16]<<16) | (headDat[17]<<24);
+	m_header.bpp = headDat[18];
+	m_header.base = headDat[19];
+	// Check if valid
+	if(m_header.id1 != 0xBF || m_header.id2 != 0xF2)
+	{
+	    in.close();
+	    LOG.error("Invalid BFF2 bitmap font file \"%\".", filename.c_str());
+	    return false;
+	}
+	// Reject unsupported bpp values
+	if(m_header.bpp != 8 || m_header.bpp != 24 || m_header.bpp != 32)
+	{
+	    in.close();
+	    LOG.error("Bitmap font file \"%\" has unsupported amount of bits per pixel.", filename.c_str());
+	    return false;
+	}
+	// Read in array with width of each character
+	in.read(&(m_widths[0]), 256);
+	// Read in texture
+	long long int texDataSize = (m_header.imgWidth * m_header.imgHeight) * (m_header.bpp / 8);
+	img = new char[texDataSize];
+	if(!img)
+	{
+	    in.close();
+	    LOG.error("Unable to allocate memory for bitmap font file \"%\".", filename.c_str());
+	    return false;
+	}
+	in.read(img, texDataSize);
+	in.close();
+
+	// Create texture
+	GLuint texId = GL_INVALID_VALUE;
+	glGenTextures(1, &texId);
+	glBindTexture(GL_TEXTURE_2D, texId);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// Tex creation params are dependent on BPP
+	switch (m_header.bpp)
+	{
+	    case 8:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, m_header.imgWidth, m_header.imgHeight, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, img);
+		break;
+	    case 24:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_header.imgWidth, m_header.imgHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+		break;
+	    case 32:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_header.imgWidth, m_header.imgHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+		break;
+	    default:
+		// Should never get here
+		LOG.error("Bitmap font file \"%\" corrupt.", filename.c_str());
+		break;
+	}
+	m_pTexture = new Texture {texId};
+	m_pSprite = new Sprite {m_pTexture};
+
+	delete [] img;
+	return true;
+    }
+}
