12645cbc10fb010f150ac27da6f55e5499d44183
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-10-26 23:57:18 +0100

    Implemented bmp writing

--- DBGL_Resources/plugins/Texture/BMP/BMPModule.cpp
+++ DBGL_Resources/plugins/Texture/BMP/BMPModule.cpp
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <algorithm>
+#include <fstream>
 #include "DBGL/Resources/Texture/IIMageFormatModule.h"
 
 namespace dbgl
@@ -17,6 +18,29 @@ namespace dbgl
     class BMPModule : public IImageFormatModule
     {
 	public:
+	    struct FileHeaderBMP
+	    {
+		    uint16_t id = 0x4D42;
+		    uint32_t fileSize = 0;
+		    uint32_t res = 0;
+		    uint32_t off = 54;
+	    };
+
+	    struct InfoHeaderBMP
+	    {
+		    uint32_t size = 40;
+		    int32_t width = 0;
+		    int32_t height = 0;
+		    uint16_t panes = 1;
+		    uint16_t bpp = 24;
+		    uint32_t compr = 0;
+		    uint32_t imgSize = 0;
+		    int32_t xPixPerMeter = 0;
+		    int32_t yPixPerMeter = 0;
+		    uint32_t indexClr = 0;
+		    uint32_t clr = 0;
+	    };
+
 	    virtual ~BMPModule() = default;
 
 	    virtual bool canLoad() const
@@ -48,12 +72,50 @@ namespace dbgl
 
 	    virtual bool write(ITexture* tex, std::string const& path) const
 	    {
-		return false;
+		return write(tex, Filename{path});
 	    }
 
 	    virtual bool write(ITexture* tex, Filename const& path) const
 	    {
-		return false;
+		unsigned int imgDataSize { tex->getWidth() * tex->getHeight() * 3 };
+		char* bmpBuf = new char[imgDataSize];
+		if(!bmpBuf)
+		    return false;
+		// Copy pixel data to buffer
+		tex->bind(0);
+		tex->getPixelData(ITexture::PixelFormat::BGR, ITexture::PixelType::UBYTE, bmpBuf);
+		// Open file stream
+		std::ofstream file(path.get(), std::fstream::out | std::fstream::binary | std::fstream::trunc);
+		if(!file.good())
+		    return false;
+		// Write file header
+		FileHeaderBMP fileHeader{};
+		fileHeader.fileSize = imgDataSize;
+		file.write(reinterpret_cast<const char*>(&fileHeader.id), sizeof(uint16_t)); // "BM"
+		file.write(reinterpret_cast<const char*>(&fileHeader.fileSize), sizeof(uint32_t)); // File size
+		file.write(reinterpret_cast<const char*>(&fileHeader.res), sizeof(uint32_t)); // Reserved
+		file.write(reinterpret_cast<const char*>(&fileHeader.off), sizeof(uint32_t)); // Offset to image data
+		// Write info header
+		InfoHeaderBMP infoHeader{};
+		infoHeader.width = static_cast<int32_t>(tex->getWidth());
+		infoHeader.height = static_cast<int32_t>(tex->getHeight());
+		file.write(reinterpret_cast<const char*>(&infoHeader.size), sizeof(uint32_t)); // Size of info header
+		file.write(reinterpret_cast<const char*>(&infoHeader.width), sizeof(int32_t)); // Width in pixels
+		file.write(reinterpret_cast<const char*>(&infoHeader.height), sizeof(int32_t)); // Height in pixels
+		file.write(reinterpret_cast<const char*>(&infoHeader.panes), sizeof(uint16_t)); // Color planes (unused)
+		file.write(reinterpret_cast<const char*>(&infoHeader.bpp), sizeof(uint16_t)); // BPP
+		file.write(reinterpret_cast<const char*>(&infoHeader.compr), sizeof(uint32_t)); // Not compressed
+		file.write(reinterpret_cast<const char*>(&infoHeader.imgSize), sizeof(uint32_t)); // Size of image data (unused)
+		file.write(reinterpret_cast<const char*>(&infoHeader.xPixPerMeter), sizeof(int32_t)); // X pixel per meter (unused)
+		file.write(reinterpret_cast<const char*>(&infoHeader.yPixPerMeter), sizeof(int32_t)); // Y pixel per meter (unused)
+		file.write(reinterpret_cast<const char*>(&infoHeader.indexClr), sizeof(uint32_t)); // Amount of indexed colors (no indexing)
+		file.write(reinterpret_cast<const char*>(&infoHeader.clr), sizeof(uint32_t)); // Use all colors
+		// Write actual image
+		file.write(bmpBuf, imgDataSize);
+		file.close();
+		delete [] bmpBuf;
+
+		return true;
 	    }
     };
 }
