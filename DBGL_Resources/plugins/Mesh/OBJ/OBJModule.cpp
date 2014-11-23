fce3e41654bdd7d758bb4d72be89669374c3bb3b
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-11-23 18:14:15 +0100

    Implemented OBJ loader + test

--- DBGL_Resources/plugins/Mesh/OBJ/OBJModule.cpp
+++ DBGL_Resources/plugins/Mesh/OBJ/OBJModule.cpp
@@ -12,19 +12,239 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 #include "DBGL/Resources/Mesh/IMeshFormatModule.h"
+#include "DBGL/Core/Math/Vector3.h"
+#include "DBGL/Core/Math/Vector2.h"
+#include "DBGL/Core/Math/Utility.h"
 #include "DBGL/Platform/Platform.h"
 
 namespace dbgl
 {
     class OBJModule : public IMeshFormatModule
     {
+	private:
+	    /**
+	     * @brief Every OBJ face consists of three vertices having a position and (possibly)
+	     * 	      a UV and/or normal.
+	     */
+	    struct FaceComponent
+	    {
+		public:
+		    /**
+		     * @brief Index of the vertex (coordinate)
+		     */
+		    unsigned short vertexIndex = std::numeric_limits<unsigned short>::max();
+		    /**
+		     * @brief Index of the UV
+		     */
+		    unsigned short uvIndex = std::numeric_limits<unsigned short>::max();
+		    /**
+		     * @brief Index of the normal
+		     */
+		    unsigned short normalIndex = std::numeric_limits<unsigned short>::max();
+	    };
+	    /**
+	     * @brief One face (i.e. polygon)
+	     */
+	    struct Face
+	    {
+		public:
+		    /**
+		     * @brief Components of a face. Currently only polygons with 3 vertices are allowed.
+		     */
+		    FaceComponent components[3]{};
+	    };
+
+	    template <typename T> T to(std::string const& val) const
+	    {
+		std::istringstream lineStream {};
+		lineStream.str(val);
+		T ret = 0;
+		lineStream >> ret;
+		return ret;
+	    }
+	    /**
+	     * @brief Extracts data a string
+	     * @param line String to tokenize
+	     * @param[out] first First token as string
+	     * @param[out] container Other tokens as T
+	     */
+	    template <typename T> void extract(std::string const& line, std::string& first, std::vector<T>& container) const
+	    {
+		std::istringstream lineStream{};
+		lineStream.str(line);
+		lineStream >> first;
+		T curCoord{};
+		while (lineStream >> curCoord)
+		    container.push_back(curCoord);
+	    }
+	    /**
+	     * @brief Tokenizes as string
+	     * @param line Line to tokenize
+	     * @param delim Delimiter
+	     * @param container Container to append tokens to
+	     */
+	    void tokenize(std::string const& line, char delim, std::vector<std::string>& container) const
+	    {
+		std::string curElement{};
+		std::stringstream tokenStream{};
+		tokenStream.str(line);
+		while (std::getline(tokenStream, curElement, delim))
+		{
+		    if(curElement.empty())
+			continue;
+		    container.push_back(curElement);
+		}
+	    }
+	    /**
+	     * @brief Merges the raw data into a single index
+	     * @param[out] mesh Mesh to store to
+	     * @param faces Original faces
+	     * @param vertices Original vertices
+	     * @param normals Original normals
+	     * @param uvs Original uvs
+	     */
+	    void interpret(IMesh* mesh, std::vector<Face> const& faces, std::vector<Vec3f> const& vertices,
+		    std::vector<Vec3f> const& normals, std::vector<Vec2f> const& uvs) const
+	    {
+		// Iterate over all faces
+		for(unsigned int i = 0; i < faces.size(); i++)
+		{
+		    auto curFace = faces[i];
+		    // Iterate over all 3 vertices in a face
+		    for(unsigned int j = 0; j < 3; j++)
+		    {
+			Vec3f vertex, normal;
+			Vec2f uv;
+			// Get vertex
+			if(vertices.size() > curFace.components[j].vertexIndex)
+			    vertex = vertices[curFace.components[j].vertexIndex];
+			else
+			{
+			    // This should never happen TODO: Error handling
+			    return;
+			}
+			// Get UV
+			bool useUv = true;
+			if(uvs.size() > curFace.components[j].uvIndex)
+			    uv = uvs[curFace.components[j].uvIndex];
+			else
+			    useUv = false;
+			// Get normal
+			bool useNormal = true;
+			if (normals.size() > curFace.components[j].normalIndex)
+			    normal = normals[curFace.components[j].normalIndex];
+			else
+			    useNormal = false;
+			// Add new vertex, normal and uv.
+			auto vertIndex = mesh->vertices().size();
+			mesh->vertices().push_back(vertex);
+			if (useUv)
+			    mesh->uvs().push_back(uv);
+			if(useNormal)
+			    mesh->normals().push_back(normal);
+			// Add the index
+			mesh->indices().push_back(vertIndex);
+		    }
+		}
+	    }
+	    /**
+	     * @brief Analyzes the obj code
+	     * @param contents
+	     * @return
+	     */
+	    IMesh* analyze(std::string const& contents) const
+	    {
+		std::vector<Face> origFaces{};
+		std::vector<Vec3f> origVertices{};
+		std::vector<Vec3f> origNormals{};
+		std::vector<Vec2f> origUvs{};
+		std::istringstream lineStream{};
+		lineStream.str(contents);
+		std::string line{};
+		std::string type{};
+		std::vector<std::string> params;
+		while(lineStream.good())
+		{
+		    // Read line
+		    std::getline(lineStream, line);
+		    if (line.length() == 0) // Skip empty lines
+			continue;
+		    params.clear();
+		    type.clear();
+		    extract(line, type, params);
+		    if (type == "v") // Vertex
+		    {
+			if (params.size() < 3)
+			    return nullptr; // TODO: Better handling of misformatted files
+			if (params.size() > 3)
+			    return nullptr;
+			origVertices.push_back( { to<float>(params[0]), to<float>(params[1]), to<float>(params[2]) });
+		    }
+		    else if (type == "vt") // UV
+		    {
+			if (params.size() < 2)
+			    return nullptr;
+			if (params.size() > 3) // There may be a w coordinate (ignored here)
+			    return nullptr;
+			origUvs.push_back( { to<float>(params[0]), to<float>(params[1]) });
+		    }
+		    else if (type == "vn") // Normal
+		    {
+			if (params.size() < 3)
+			    return nullptr;
+			if (params.size() > 3)
+			    return nullptr;
+			origNormals.push_back( { to<float>(params[0]), to<float>(params[1]), to<float>(params[2]) });
+		    }
+		    else if (type == "f") // Face
+		    {
+			Face curFace {};
+			for (unsigned int i = 0; i < params.size(); i++)
+			{
+			    FaceComponent curFaceComponent {};
+			    std::vector<std::string> tokens {};
+			    tokenize(params[i], '/', tokens);
+			    for (unsigned int j = 0; j < tokens.size(); j++)
+			    {
+				unsigned int curIndex = to<unsigned int>(tokens[j]);
+				switch (j)
+				{
+				    case 0:
+					curFaceComponent.vertexIndex = curIndex - 1; // OBJ indices start with 1
+					break;
+				    case 1:
+					curFaceComponent.uvIndex = curIndex - 1;
+					break;
+				    case 2:
+					curFaceComponent.normalIndex = curIndex - 1;
+					break;
+				    default:
+					// TODO: Issue warning
+					break;
+				}
+			    }
+			    curFace.components[i] = curFaceComponent;
+			}
+			origFaces.push_back(curFace);
+		    }
+		    else
+			// Might be a comment or some other line we can skip
+			continue;
+		}
+		// Create mesh
+		auto mesh = Platform::get()->createMesh();
+		interpret(mesh, origFaces, origVertices, origNormals, origUvs);
+		return mesh;
+	    }
+
 	public:
 	    virtual ~OBJModule() = default;
 
 	    virtual bool canLoad() const
 	    {
-		return false;
+		return true;
 	    }
 
 	    virtual bool canWrite() const
@@ -46,7 +266,24 @@ namespace dbgl
 
 	    virtual IMesh* load(Filename const& path) const
 	    {
-		return nullptr;
+		// Read file
+		std::ifstream file;
+		file.open(path.get().c_str(), std::ios::in);
+		std::string contents;
+		if (file.is_open())
+		{
+		    file.seekg(0, std::ios::end);
+		    contents.resize(file.tellg());
+		    file.seekg(0, std::ios::beg);
+		    file.read(&contents[0], contents.size());
+		}
+		// Close file
+		file.close();
+
+		// Analyze contents
+		auto mesh = analyze(contents);
+
+		return mesh;
 	    }
 
 	    virtual bool write(IMesh* mesh, std::string const& path) const
@@ -54,7 +291,7 @@ namespace dbgl
 		return write(mesh, Filename{path});
 	    }
 
-	    virtual bool write(IMesh* mesh, Filename const& path) const
+	    virtual bool write(IMesh* /* mesh */, Filename const& /* path */) const
 	    {
 		return false;
 	    }
