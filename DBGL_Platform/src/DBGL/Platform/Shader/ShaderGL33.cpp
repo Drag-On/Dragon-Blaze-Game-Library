7bbd62fc84f9c6c2c446999ec8fbd03ad87b6407
Drag-On <drag-on@gmx.de>
2015-03-18 18:52:02 +0100

    Improved exception handling, formatting

--- DBGL_Platform/src/DBGL/Platform/Shader/ShaderGL33.cpp
+++ DBGL_Platform/src/DBGL/Platform/Shader/ShaderGL33.cpp
@@ -12,67 +12,68 @@
 
 namespace dbgl
 {
-    ShaderGL33::ShaderGL33(Type type, std::string code) : m_type{type}, m_code{code}
-    {
-    }
-
-    ShaderGL33::~ShaderGL33()
-    {
-	glDeleteShader(m_id);
-    }
-
-    void ShaderGL33::compile()
-    {
-	if (m_code.length() == 0)
-	    return;
-
-	// In case the shader has already been compiled -> delete old result
-	if(m_id != 0)
-	    glDeleteShader(m_id);
-
-	// Create shader object
-	m_id = glCreateShader(shaderType2GL(m_type));
-
-	// Compile
-	const char* codePtr = m_code.c_str();
-	glShaderSource(m_id, 1, &codePtr, nullptr);
-	glCompileShader(m_id);
-
-	// Check if everything went right
-	GLint result = GL_FALSE;
-	glGetShaderiv(m_id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE)
+	ShaderGL33::ShaderGL33(Type type, std::string code)
+			: m_type { type }, m_code { code }
 	{
-	    GLint logLength {0};
-	    glGetShaderiv(m_id, GL_INFO_LOG_LENGTH, &logLength);
-	    char* msg = new char[logLength] {};
-	    // Get actual log
-	    glGetShaderInfoLog(m_id, logLength, nullptr, msg);
-	    std::string message{msg};
-	    delete msg;
-	    throw std::runtime_error {message};
 	}
-    }
 
-    GLuint ShaderGL33::getHandle() const
-    {
-	return m_id;
-    }
-
-    GLenum ShaderGL33::shaderType2GL(Type type)
-    {
-	switch(type)
+	ShaderGL33::~ShaderGL33()
 	{
-	    case Type::COMPUTE:
-		return GL_COMPUTE_SHADER;
-	    case Type::FRAGMENT:
-		return GL_FRAGMENT_SHADER;
-	    case Type::GEOMETRY:
-		return GL_GEOMETRY_SHADER;
-	    case Type::VERTEX:
-		return GL_VERTEX_SHADER;
-	    default:
-		return GL_INVALID_ENUM;
+		glDeleteShader(m_id);
 	}
-    }
+
+	void ShaderGL33::compile()
+	{
+		if (m_code.length() == 0)
+			throw std::runtime_error { "No code to compile." };
+
+		// In case the shader has already been compiled -> delete old result
+		if (m_id != 0)
+			glDeleteShader(m_id);
+
+		// Create shader object
+		m_id = glCreateShader(shaderType2GL(m_type));
+
+		// Compile
+		const char* codePtr = m_code.c_str();
+		glShaderSource(m_id, 1, &codePtr, nullptr);
+		glCompileShader(m_id);
+
+		// Check if everything went right
+		GLint result = GL_FALSE;
+		glGetShaderiv(m_id, GL_COMPILE_STATUS, &result);
+		if (result == GL_FALSE)
+		{
+			GLint logLength { 0 };
+			glGetShaderiv(m_id, GL_INFO_LOG_LENGTH, &logLength);
+			char* msg = new char[logLength] { };
+			// Get actual log
+			glGetShaderInfoLog(m_id, logLength, nullptr, msg);
+			std::string message { msg };
+			delete msg;
+			throw std::runtime_error { message };
+		}
+	}
+
+	GLuint ShaderGL33::getHandle() const
+	{
+		return m_id;
+	}
+
+	GLenum ShaderGL33::shaderType2GL(Type type)
+	{
+		switch (type)
+		{
+		case Type::COMPUTE:
+			return GL_COMPUTE_SHADER;
+		case Type::FRAGMENT:
+			return GL_FRAGMENT_SHADER;
+		case Type::GEOMETRY:
+			return GL_GEOMETRY_SHADER;
+		case Type::VERTEX:
+			return GL_VERTEX_SHADER;
+		default:
+			return GL_INVALID_ENUM;
+		}
+	}
 }
