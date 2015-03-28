9c1c30266f45f2e018af102ebb6b5aa161a24a25
Drag-On <drag-on@gmx.de>
2015-03-28 17:17:45 +0100

    Added width and height getters for csv tables

--- DBGL_Core/include/DBGL/Core/Parsers/CSV.h
+++ DBGL_Core/include/DBGL/Core/Parsers/CSV.h
@@ -22,80 +22,90 @@
 
 namespace dbgl
 {
-    /**
-     * @brief Parser for csv tables
-     */
-    class CSV
-    {
+	/**
+	 * @brief Parser for csv tables
+	 */
+	class CSV
+	{
 	public:
-	    /**
-	     * @brief Loads a csv file from hard disk
-	     * @param path Path of the file to load
-	     */
-	    CSV(std::string const& path);
-	    /**
-	     * @brief Writes the csv contents back to file
-	     * @return True in case the file was written, otherwise false
-	     */
-	    bool write();
-	    /**
-	     * @brief Writes the csv contents back to file
-	     * @param path Path to write to
-	     * @return True in case the file was written, otherwise false
-	     */
-	    bool write(std::string const& path);
-	    /**
-	     * @brief Manually sets a property, overwriting any previous values
-	     * @param x X coordinate of the value to set
-	     * @param y Y coordinate of the value to set
-	     * @param value Value of the property
-	     */
-	    void setValue(unsigned int x, unsigned int y, std::string const& value);
-	    /**
-	     * @param x X coordinate of the value to get
-	     * @param y Y coordinate of the value to get
-	     * @return Raw string value associated with the passed key
-	     */
-	    std::string getStringValue(unsigned int x, unsigned int y);
-	    /**
-	     * @brief Interprets the string associated with the passed coordinates as an integer
-	     * @param x X coordinate of the value to get
-	     * @param y Y coordinate of the value to get
-	     * @return Integer value associated with the passed key
-	     */
-	    int getIntValue(unsigned int x, unsigned int y);
-	    /**
-	     * @brief Interprets the string associated with the passed coordinates as a float
-	     * @param x X coordinate of the value to get
-	     * @param y Y coordinate of the value to get
-	     * @return Float value associated with the passed key
-	     */
-	    float getFloatValue(unsigned int x, unsigned int y);
-	    /**
-	     * @brief Interprets the string associated with the passed coordinates as a boolean
-	     * @param x X coordinate of the value to get
-	     * @param y Y coordinate of the value to get
-	     * @return Boolean value associated with the passed key
-	     */
-	    bool getBoolValue(unsigned int x, unsigned int y);
-	    /**
-	     * @param delimiter String to delimit key and value in property files
-	     */
-	    void setDelimiter(std::string const& delimiter);
+		/**
+		 * @brief Loads a csv file from hard disk
+		 * @param path Path of the file to load
+		 */
+		CSV(std::string const& path);
+		/**
+		 * @brief Writes the csv contents back to file
+		 * @return True in case the file was written, otherwise false
+		 */
+		bool write();
+		/**
+		 * @brief Writes the csv contents back to file
+		 * @param path Path to write to
+		 * @return True in case the file was written, otherwise false
+		 */
+		bool write(std::string const& path);
+		/**
+		 * @brief Manually sets a property, overwriting any previous values
+		 * @param x X coordinate of the value to set
+		 * @param y Y coordinate of the value to set
+		 * @param value Value of the property
+		 */
+		void setValue(unsigned int x, unsigned int y, std::string const& value);
+		/**
+		 * @param x X coordinate of the value to get
+		 * @param y Y coordinate of the value to get
+		 * @return Raw string value associated with the passed key
+		 */
+		std::string getStringValue(unsigned int x, unsigned int y) const;
+		/**
+		 * @brief Interprets the string associated with the passed coordinates as an integer
+		 * @param x X coordinate of the value to get
+		 * @param y Y coordinate of the value to get
+		 * @return Integer value associated with the passed key
+		 */
+		int getIntValue(unsigned int x, unsigned int y) const;
+		/**
+		 * @brief Interprets the string associated with the passed coordinates as a float
+		 * @param x X coordinate of the value to get
+		 * @param y Y coordinate of the value to get
+		 * @return Float value associated with the passed key
+		 */
+		float getFloatValue(unsigned int x, unsigned int y) const;
+		/**
+		 * @brief Interprets the string associated with the passed coordinates as a boolean
+		 * @param x X coordinate of the value to get
+		 * @param y Y coordinate of the value to get
+		 * @return Boolean value associated with the passed key
+		 */
+		bool getBoolValue(unsigned int x, unsigned int y) const;
+		/**
+		 * @param delimiter String to delimit key and value in property files
+		 */
+		void setDelimiter(std::string const& delimiter);
+		/**
+		 * @brief Gets the width of a line
+		 * @param y Line to get width for
+		 * @return Amount of elements in line \p y
+		 */
+		unsigned int getWidth(unsigned int y) const;
+		/**
+		 * @brief Gets the height of a column
+		 * @param x Column to get height for
+		 * @return Amount of elements in column \p x
+		 */
+		unsigned int getHeight(unsigned int x) const;
 	private:
-	    /**
-	     * @brief Loads the specified csv file into RAM
-	     * @param path Path of the file to use
-	     * @return True in case the file could be loaded, otherwise false
-	     */
-	    bool read(std::string const& path);
+		/**
+		 * @brief Loads the specified csv file into RAM
+		 * @param path Path of the file to use
+		 * @return True in case the file could be loaded, otherwise false
+		 */
+		bool read(std::string const& path);
 
-	    std::string m_filename;
-	    std::string m_delimiter = ";";
-	    std::vector<std::vector<std::string>> m_table{};
-    };
+		std::string m_filename;
+		std::string m_delimiter = ";";
+		std::vector<std::vector<std::string>> m_table { };
+	};
 }
-
-
 
 #endif /* CSV_H_ */
--- DBGL_Core/src/DBGL/Core/Parsers/CSV.cpp
+++ DBGL_Core/src/DBGL/Core/Parsers/CSV.cpp
@@ -13,134 +13,140 @@
 
 namespace dbgl
 {
-    CSV::CSV(std::string const& path) : m_filename{path}
-    {
-	if(!read(path))
-	    throw std::runtime_error{"Unable to load file \"" + path + "\"."};
-    }
-
-    bool CSV::read(std::string const& path)
-    {
-	// Open file
-	std::ifstream file {};
-	file.open(path.c_str(), std::ios::in);
-	if (file.is_open())
+	CSV::CSV(std::string const& path)
+			: m_filename { path }
 	{
-	    // Seek to beginning
-	    file.seekg(0, std::ios::beg);
-	    int lineNo { -1 };
-	    // Scan whole file
-	    while (file.good())
-	    {
-		// Increase line number
-		lineNo++;
-		// Read line
-		std::string line {};
-		std::getline(file, line);
-		// Skip empty lines
-		if (line.length() == 0)
-		    continue;
-		// Split on delimiter
-		auto start = 0U;
-		auto end = line.find(m_delimiter);
-		std::vector<std::string> tokens {};
-		while (end != std::string::npos)
+		if (!read(path))
+			throw std::runtime_error { "Unable to load file \"" + path + "\"." };
+	}
+
+	bool CSV::read(std::string const& path)
+	{
+		// Open file
+		std::ifstream file { };
+		file.open(path.c_str(), std::ios::in);
+		if (file.is_open())
 		{
-		    tokens.push_back(line.substr(start, end - start));
-		    start = end + m_delimiter.length();
-		    end = line.find(m_delimiter, start);
+			// Seek to beginning
+			file.seekg(0, std::ios::beg);
+			int lineNo { -1 };
+			// Scan whole file
+			while (file.good())
+			{
+				// Increase line number
+				lineNo++;
+				// Read line
+				std::string line { };
+				std::getline(file, line);
+				// Skip empty lines
+				if (line.length() == 0)
+					continue;
+				// Split on delimiter
+				auto start = 0U;
+				auto end = line.find(m_delimiter);
+				std::vector<std::string> tokens { };
+				while (end != std::string::npos)
+				{
+					tokens.push_back(line.substr(start, end - start));
+					start = end + m_delimiter.length();
+					end = line.find(m_delimiter, start);
+				}
+				tokens.push_back(line.substr(start, end));
+				// Add element
+				m_table.push_back(tokens);
+			}
+			file.close();
+			return true;
 		}
-		tokens.push_back(line.substr(start, end));
-		// Add element
-		m_table.push_back(tokens);
-	    }
-	    file.close();
-	    return true;
+		Log::getDefault().warning("CSV file \"%\" could not be opened!", path.c_str());
+		return false;
 	}
-	Log::getDefault().warning("CSV file \"%\" could not be opened!", path.c_str());
-	return false;
-    }
 
-    bool CSV::write()
-    {
-	return write(m_filename);
-    }
-
-    bool CSV::write(std::string const& path)
-    {
-	// Write to some other file
-	std::ofstream outFile {};
-	outFile.open(path, std::ios::out);
-	if (outFile.is_open())
+	bool CSV::write()
 	{
-	    // Write table contents
-	    for(unsigned int y = 0; y < m_table.size(); y++)
-		for(unsigned int x = 0; x < m_table[y].size(); x++)
-		    outFile << m_table[y][x] << m_delimiter << "\n";
-	    outFile.close();
-	    return true;
+		return write(m_filename);
 	}
-	Log::getDefault().warning("CSV file \"%\" could not be opened!", path);
-	return false;
-    }
 
-    void CSV::setValue(unsigned int x, unsigned int y, std::string const& value)
-    {
-	if(m_table.size() <= y || m_table[y].size() <= x)
-	    throw std::out_of_range {
-	    "(" + std::to_string(x) + "," + std::to_string(y) + ") out of range in CSV table read from \""
-		    + m_filename + "\"."
-	    };
-	m_table[y][x] = value;
-    }
+	bool CSV::write(std::string const& path)
+	{
+		// Write to some other file
+		std::ofstream outFile { };
+		outFile.open(path, std::ios::out);
+		if (outFile.is_open())
+		{
+			// Write table contents
+			for (unsigned int y = 0; y < m_table.size(); y++)
+				for (unsigned int x = 0; x < m_table[y].size(); x++)
+					outFile << m_table[y][x] << m_delimiter << "\n";
+			outFile.close();
+			return true;
+		}
+		Log::getDefault().warning("CSV file \"%\" could not be opened!", path);
+		return false;
+	}
 
-    std::string CSV::getStringValue(unsigned int x, unsigned int y)
-    {
-	if(m_table.size() <= y || m_table[y].size() <= x)
-	    throw std::out_of_range {
-	    "(" + std::to_string(x) + "," + std::to_string(y) + ") out of range in CSV table read from \""
-		    + m_filename + "\"."
-	    };
-	return m_table[y][x];
-    }
+	void CSV::setValue(unsigned int x, unsigned int y, std::string const& value)
+	{
+		if (m_table.size() <= y || m_table[y].size() <= x)
+			throw std::out_of_range { "(" + std::to_string(x) + "," + std::to_string(y)
+					+ ") out of range in CSV table read from \"" + m_filename + "\"." };
+		m_table[y][x] = value;
+	}
 
-    int CSV::getIntValue(unsigned int x, unsigned int y)
-    {
-	if(m_table.size() <= y || m_table[y].size() <= x)
-	    throw std::out_of_range {
-	    "(" + std::to_string(x) + "," + std::to_string(y) + ") out of range in CSV table read from \""
-		    + m_filename + "\"."
-	    };
-	return atoi(m_table[y][x].c_str());
-    }
+	std::string CSV::getStringValue(unsigned int x, unsigned int y) const
+	{
+		if (m_table.size() <= y || m_table[y].size() <= x)
+			throw std::out_of_range { "(" + std::to_string(x) + "," + std::to_string(y)
+					+ ") out of range in CSV table read from \"" + m_filename + "\"." };
+		return m_table[y][x];
+	}
 
-    float CSV::getFloatValue(unsigned int x, unsigned int y)
-    {
-	if(m_table.size() <= y || m_table[y].size() <= x)
-	    throw std::out_of_range {
-	    "(" + std::to_string(x) + "," + std::to_string(y) + ") out of range in CSV table read from \""
-		    + m_filename + "\"."
-	    };
-	return atof(m_table[y][x].c_str());
-    }
+	int CSV::getIntValue(unsigned int x, unsigned int y) const
+	{
+		if (m_table.size() <= y || m_table[y].size() <= x)
+			throw std::out_of_range { "(" + std::to_string(x) + "," + std::to_string(y)
+					+ ") out of range in CSV table read from \"" + m_filename + "\"." };
+		return atoi(m_table[y][x].c_str());
+	}
 
-    bool CSV::getBoolValue(unsigned int x, unsigned int y)
-    {
-	if(m_table.size() <= y || m_table[y].size() <= x)
-	    throw std::out_of_range {
-	    "(" + std::to_string(x) + "," + std::to_string(y) + ") out of range in CSV table read from \""
-		    + m_filename + "\"."
-	    };
-	std::string val = m_table[y][x];
-	bool b {};
-	std::istringstream(val) >> std::boolalpha >> b;
-	return b;
-    }
+	float CSV::getFloatValue(unsigned int x, unsigned int y) const
+	{
+		if (m_table.size() <= y || m_table[y].size() <= x)
+			throw std::out_of_range { "(" + std::to_string(x) + "," + std::to_string(y)
+					+ ") out of range in CSV table read from \"" + m_filename + "\"." };
+		return atof(m_table[y][x].c_str());
+	}
 
-    void CSV::setDelimiter(std::string const& delimiter)
-    {
-	m_delimiter = delimiter;
-    }
+	bool CSV::getBoolValue(unsigned int x, unsigned int y) const
+	{
+		if (m_table.size() <= y || m_table[y].size() <= x)
+			throw std::out_of_range { "(" + std::to_string(x) + "," + std::to_string(y)
+					+ ") out of range in CSV table read from \"" + m_filename + "\"." };
+		std::string val = m_table[y][x];
+		bool b { };
+		std::istringstream(val) >> std::boolalpha >> b;
+		return b;
+	}
+
+	void CSV::setDelimiter(std::string const& delimiter)
+	{
+		m_delimiter = delimiter;
+	}
+
+	unsigned int CSV::getWidth(unsigned int y) const
+	{
+		if (m_table.size() <= y)
+			return 0;
+		else
+			return m_table[y].size();
+	}
+
+	unsigned int CSV::getHeight(unsigned int x) const
+	{
+		if (getWidth(0) < x)
+			return 0;
+		else
+			return m_table.size();
+	}
 }
-
 
